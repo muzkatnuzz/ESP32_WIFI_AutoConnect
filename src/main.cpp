10ab94e4299041bb556e69860c00362d6cd32e5c
muzkatnuzz <muzkatnuzz@outlook.com>
2021-05-26 22:17:25 +0200

    handle not found in main

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,51 @@ void handleTest(AsyncWebServerRequest *request)
 void handleReset(AsyncWebServerRequest *request)
 {
   request->send(200, "plain/text", "Handle reset. About to restart...");
-  
-  // reset wifi settings (remove credentials) 
-  ESPAsync_wifiManager.resetSettings();   //reset saved settings
-  
+
+  // reset wifi settings (remove credentials)
+  ESPAsync_wifiManager.resetSettings(); //reset saved settings
+
   ESP.restart();
   delay(2000);
+}
+
+void handleNotFound(AsyncWebServerRequest *request)
+{
+  String message = "File Not Found\n\n";
+
+  message += "URI: ";
+  message += request->url();
+  message += "\nMethod: ";
+  message += (request->method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += request->args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < request->args(); i++)
+  {
+    message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
+  }
+
+  message += "\nHeaders: ";
+  message += request->headers();
+  message += "\n";
+
+  for (uint8_t i = 0; i < request->headers(); i++)
+  {
+    message += " " + request->getHeader(i)->name() + ": " + request->getHeader(i)->value() + "\n";
+  }
+
+  AsyncWebServerResponse *response = request->beginResponse(404, "text/plain", message);
+  response->addHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+
+#if USING_CORS_FEATURE
+  // New from v1.1.0, for configure CORS Header, default to WM_HTTP_CORS_ALLOW_ALL = "*"
+  response->addHeader("Access-Control-Allow-Origin", "*");
+#endif
+
+  response->addHeader("Pragma", "no-cache");
+  response->addHeader("Expires", "-1");
+  request->send(response);
 }
 
 void setup()
@@ -45,7 +84,8 @@ void setup()
 
   webServer.on("/test", HTTP_GET, handleTest);
   webServer.on("/reset", HTTP_GET, handleReset);
-  
+  webServer.onNotFound(handleNotFound);
+
   webServer.begin(); // Web server start in case of STA wifi connection
 }
 
